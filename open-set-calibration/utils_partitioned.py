311315b3078a38b03d9075cf0ede99c092a6051f
lvzongyao <lv_zongyao@163.com>
2022-05-06 00:33:12 -0500

    partitioned utils functions

--- open-set-calibration/utils_partitioned.py
+++ open-set-calibration/utils_partitioned.py
@@ -146,36 +146,36 @@ def get_test_loader(target='cifar100', root='E:/Datasets/CIFAR10'):
     return loader
 
 
-def get_model(name, num_classes, num_channels):
-    """ return model architecture for image recognition
-
-    Args:
-        name: which model architecture to use
-        num_classes: number of classes for classification
-        pretrained: whether or not to get imagenet pretrained model
-
-    Return:
-        model: neural network object to be used
-    """
-    if name == 'resnet18':
-        # model = models.resnet18
-        model = resnet18 if num_channels == 3 else resnet1.resnet18
-    elif name == 'resnet34':
-        # model = models.resnet34
-        model = resnet34 if num_channels == 3 else resnet1.resnet34
-    elif name == 'resnet50':
-        # model = models.resnet50
-        model = resnet50 if num_channels == 3 else resnet1.resnet50
-    elif name == 'resnet101':
-        # model = models.resnet101
-        model = resnet101 if num_channels == 3 else resnet1.resnet101
-    elif name == 'resnet110':
-        model = resnet110 if num_channels == 3 else resnet1.resnet110
-    elif name == 'resnet152':
-        # model = models.resnet152
-        model = resnet152 if num_channels == 3 else resnet1.resnet152
-
-    return model(num_classes=num_classes)
+# def get_model(name, num_classes, num_channels):
+#     """ return model architecture for image recognition
+#
+#     Args:
+#         name: which model architecture to use
+#         num_classes: number of classes for classification
+#         pretrained: whether or not to get imagenet pretrained model
+#
+#     Return:
+#         model: neural network object to be used
+#     """
+#     if name == 'resnet18':
+#         # model = models.resnet18
+#         model = resnet18 if num_channels == 3 else resnet1.resnet18
+#     elif name == 'resnet34':
+#         # model = models.resnet34
+#         model = resnet34 if num_channels == 3 else resnet1.resnet34
+#     elif name == 'resnet50':
+#         # model = models.resnet50
+#         model = resnet50 if num_channels == 3 else resnet1.resnet50
+#     elif name == 'resnet101':
+#         # model = models.resnet101
+#         model = resnet101 if num_channels == 3 else resnet1.resnet101
+#     elif name == 'resnet110':
+#         model = resnet110 if num_channels == 3 else resnet1.resnet110
+#     elif name == 'resnet152':
+#         # model = models.resnet152
+#         model = resnet152 if num_channels == 3 else resnet1.resnet152
+#
+#     return model(num_classes=num_classes)
 
 
 def get_optim(name, model):
